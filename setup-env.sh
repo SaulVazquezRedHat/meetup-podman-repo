236879c611ae910b15d277dc7cf6522df0debf1f
Saul Vazquez <saul.vazquez.vidal@gmail.com>
2023-08-28 17:38:19 -0600

    Modificación a setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Se instala Podman
-yum install podman -y && \
+yum install podman -y
 
 # Se configuran los registries
-echo "unqualified-search-registries = ["quay.io", "registry.access.redhat.com", "registry.redhat.io", "docker.io"]" > /etc/containers/registries.conf && \
+echo "unqualified-search-registries = ["quay.io", "registry.access.redhat.com", "registry.redhat.io", "docker.io"]" > /etc/containers/registries.conf
 
 # Se cambia a usuario no-root
-su rhel && \
-cd /home/rhel && clear
+echo "redhat" | sudo -S sleep 1 && sudo su - rhel
