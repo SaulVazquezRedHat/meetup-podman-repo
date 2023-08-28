20efc26e24e66d7c4ec95d53eb90de06b8c0ccbd
Saul Vazquez <saul.vazquez.vidal@gmail.com>
2023-08-28 17:51:39 -0600

    Modificación a setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -4,13 +4,13 @@
 yum install podman -y
 
 # Se configuran los registries
-echo "unqualified-search-registries = ["quay.io", "registry.access.redhat.com", "registry.redhat.io", "docker.io"]" > /etc/containers/registries.conf
+echo 'unqualified-search-registries = ["quay.io", "registry.access.redhat.com", "registry.redhat.io", "docker.io"]' > /etc/containers/registries.conf
 
 # Se descarga imagen hello-world
 podman pull quay.io/rh-ee-savazque/hello-world:latest
 
 # Se clona el repo del meetup
-git clone https://github.com/SaulVazquezRedHat/meetup-podman-repo.git /home/rhel/
+git clone https://github.com/SaulVazquezRedHat/meetup-podman-repo.git /home/rhel/meetup-podman-repo
 
 # Se cambia a usuario no-root
 echo "redhat" | sudo -S sleep 1 && sudo su - rhel
