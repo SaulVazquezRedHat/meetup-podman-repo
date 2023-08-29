30a5cd5969eab20df0b606dabc479b68a7dec414
Saul Vazquez <saul.vazquez.vidal@gmail.com>
2023-08-28 18:12:31 -0600

    Modificación a setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -6,6 +6,8 @@ yum install podman -y
 # Se configuran los registries
 echo 'unqualified-search-registries = ["quay.io", "registry.access.redhat.com", "registry.redhat.io", "docker.io"]' > /etc/containers/registries.conf
 
+rm -rf /home/rhel/meetup-podman-rhel
+
 # Se descarga imagen hello-world
 su -c 'podman pull quay.io/rh-ee-savazque/hello-world:latest' rhel
 
